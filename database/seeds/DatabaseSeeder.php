243fa13d8d7cc9a55ed6d6c7c2f0c5de44775a3b
MUHAMMADSHOAIB\Muhammad Shoaib <mohammadshoaibwardak@gmailcom>
2017-05-03 22:50:55 +0500

    Initially Commit.

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(CountriesTableSeeder::class);
+        $this->call(StatesTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
+        
+    }
+}
